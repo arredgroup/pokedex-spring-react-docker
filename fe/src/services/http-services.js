49c3be9e657b17cb72f65c9a93d0ce8fd5ebc0b7
Christopher Arredondo <arredgroup@gmail.com>
2021-06-16 20:09:06 -0400

    added URL Production

--- fe/src/services/http-services.js
+++ fe/src/services/http-services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/api/pokemon",
+    baseURL: "http://35.232.192.5:8080/api/pokemon",
     headers: {
         "Content-type": "application/json"
     }
